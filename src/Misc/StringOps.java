83b289df7f58067d71bfd413f125079219d5d4f9
Devyani Sharma <devyani.may@gmail.com>
2024-08-13 17:54:55 -0700

    Reverse string

--- src/Misc/StringOps.java
+++ src/Misc/StringOps.java
@@ -1,2 +1,35 @@
-package Misc;public class StringOps {
+package Misc;
+
+import java.util.HashSet;
+
+public class StringOps {
+
+    public static boolean containsDuplicate( int[] nums) {
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num: nums) {
+            if (seen.contains(num)) {
+                System.out.println(seen);
+                return true;
+            }
+            seen.add(num);
+        }
+    return false;
+    }
+
+    public static String reverseString(String str) {
+        StringBuilder builder = new StringBuilder();
+        for (int i=str.length()-1; i>=0;i--) {
+            builder.append(str.charAt(i));
+        }
+        return builder.toString();
+    }
+
+
+    public static void main(String[] args) {
+        String str = "abracadabra";
+
+        System.out.println(reverseString(str));
+    }
 }
---
+++ src/Strings/ReverseString.java
@@ -0,0 +1,30 @@
+package Strings;
+
+/**
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ */
+public class ReverseString {
+
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
+
+        while(left<right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+        System.out.println(s);
+    }
+
+
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+        System.out.println();
+    }
+}
